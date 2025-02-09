4f1063874303ad58c3368e5e67375ebdd5aafcda
Fredrick Denga <dengafred@gmail.com>
2025-02-09 13:11:51 -0800

    develop liveview form for create_gist

--- lib/debut_web/live/create_gist_live.ex
+++ lib/debut_web/live/create_gist_live.ex
@@ -1,8 +1,13 @@
 defmodule DebutWeb.CreateGistLive do
   use DebutWeb, :live_view
+  alias Debut.{Gists, Gists.Gist}
   # call_back_fn -> mount, render, handle
 
   def mount(_params, _session, socket) do
+    socket = assign(
+      socket,
+      form: to_form(Gists.change_gist(%Gist{}))
+    )
     {:ok, socket}
   end
 
